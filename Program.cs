69211b9cbdc0e44161ea5992eeb9db016f458f39
Sergey Senin <TUGARINKoder@yandex.ru>
2025-07-03 23:05:12 +0300

    Добавлена стартовая локация и ее описание

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace DnD_8_bits_of_DESPAIR
     {
         static void Main(string[] args)
         {
-            TechnicalSettings.SetWindowSize();
+            //TechnicalSettings.SetWindowSize();
+            Console.WindowHeight = 40;
 
             string selectRace = "";
             BaseCharacter playerHero = new BaseCharacter();
@@ -18,19 +19,13 @@ namespace DnD_8_bits_of_DESPAIR
             UserText.DisplayWelcomeMessage();
             UserText.RaceOptionMessage();
             Console.WriteLine();
-            Console.WriteLine("* * * * * * * * * * * * * * * * * * * * * * * * * * * *");
-            Console.Write("Итак твой выбор:");
+            Console.WriteLine("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+            Console.Write("Давай определимся кто ты:");
             Console.WriteLine();
 
             selectRace = Console.ReadLine();
             UserText.UserChoiceRace(playerHero, selectRace);
             Console.WriteLine();
-
-            TechnicalSettings.ColorMessage($"Прекрасный выбор! А теперь немного статистики: \n* 87% таких же {selectRace} померли в первой же комнате " +
-                $"\n* 10% сошли с ума, глядя на происходящее тут " +
-                $"\n* 3% стали мемами. Шансы - как найти трезвого гнома!", ConsoleColor.DarkYellow);
-
-
 
             TechnicalSettings.ColorMessage("О, великий безымянный источник разочарования! " +
                 "Дай же имя своему будущему провалу. Помни: оно будет высечено на надгробии... " +
@@ -40,14 +35,9 @@ namespace DnD_8_bits_of_DESPAIR
 
             UserText.NameProcessing(playerHero);
 
-            TechnicalSettings.ColorMessage("Перед тобой комната. На стенах - пятна плесени в форме политиков твоего королевства." +
-                "\nНа полу: кости (80% крысы, 20% твой предшественник)." +
-                "\nЗапах: смесь сыра 'Дор Блю' и чьей-то нереализованной мечты.", ConsoleColor.DarkYellow);
-            TechnicalSettings.ColorMessage("Видишь дверь? Она криво висит, как твои жизненные приоритеты.\nВарианты:\n" +
-                "\n1 - Пнуть ее (шанс 95% получить занозу)" +
-                "\n\n2 - Вежливо попросить открыться (шанс 0%, но зато ты вежливый труп)", ConsoleColor.DarkYellow);
-
-            TechnicalSettings.ColorMessage("Что выбираешь?");
+            FirstLocation.GetFirstLocationDescription();
+            Console.WriteLine();
+            FirstLocation.GetHeroStartedInventory();
 
             Console.ReadKey();
         }
--- UserText.cs
+++ UserText.cs
@@ -4,6 +4,55 @@ namespace DnD_8_bits_of_DESPAIR
 {
     internal class UserText
     {
+        public static void DisplayWelcomeMessage()
+        {
+            TechnicalSettings.ColorMessage("ПРИВЕТСТВУЮ, БУДУЩИЙ ТРУП! Я - ДУХ КОНСОЛИ, ТВОЙ ГИД В ЦИФРОВОМ АДУ. \nДАВАЙ СОЗДАДИМ ПЕРСОНАЖА КОТОРЫЙ ПОМРЕТ ВСРАТОЙ СМЕРТЬЮ!");
+            TechnicalSettings.ColorMessage("\nВЫБОР РАСЫ:");
+        }
+
+        public static void RaceOptionMessage()
+        {
+            TechnicalSettings.ColorMessage("\n1. ЧЕЛОВЕК - скучно, но универсально. Как латте.\n" +
+                "2. ЭЛЬФ - если хочешь быть изящным трупом. \n" +
+                "3. ДВАРФ - идеален для тех, кто мечтает о роскошном надгробии.", ConsoleColor.DarkYellow);
+        }
+
+        public static void UserChoiceRace(BaseCharacter playerHero, string selectRace)
+        {
+            switch (selectRace)
+            {
+                case "1":
+                    TechnicalSettings.ColorMessage("Ах, классика! Человек - существо, которое умудрилось изобрести колесо и кредиты в одном тысячелетии. " +
+                        "Твои главные таланты: умирать от бумажных порезов и размножаться в геометрической прогрессии." +
+                        " Надеюсь, ты принес вино? Ну тогда хотя бы умри эффектно!");
+                    selectRace = "людей";
+                    playerHero = new Human();
+                    break;
+                case "2":
+                    TechnicalSettings.ColorMessage("Ооо, изящный ушастый экспонат в лосинах! Ты так прекрасен, что даже мхи твоей могилы будут расти узором. " +
+                        "Помни: если проживешь 500 лет и не скажешь ни одного умного слова - это не сранная деменция, а традиция. " +
+                        "Спой нам песенку о вегетарианстве, пока гоблин готовит тебе клизму из стрелы!");
+                    selectRace = "эльфов";
+                    playerHero = new Elf();
+                    break;
+                case "3":
+                    TechnicalSettings.ColorMessage("*Откашливаюсь от пыли* " +
+                        "О, бородатый холодильник на ножках! Твои предки копали так глубоко, что однажды докопались до ада...и попросили скидку на уголь. " +
+                        "Совет: если застрянешь в тоннеле - не зови на помощь. " +
+                        "Так ты станешь вечным украшением семейного древа.");
+                    selectRace = "дварфов";
+                    playerHero = new Dwarf();
+                    break;
+                default:
+
+                    break;
+            }
+
+            TechnicalSettings.ColorMessage($"Прекрасный выбор! А теперь немного статистики: \n* 87% таких же {selectRace} померли в первой же комнате " +
+                $"\n* 10% сошли с ума, глядя на происходящее тут " +
+                $"\n* 3% стали мемами. Шансы - как найти трезвого гнома!", ConsoleColor.DarkYellow);
+        }
+
         public static void NameProcessing(BaseCharacter nameHero)
         {
             int lengthName = nameHero.name.Length;
@@ -58,51 +107,6 @@ namespace DnD_8_bits_of_DESPAIR
                         $"\n-Максимальное достижение — смерть от крысиного чиха," +
                         $"\n-Титул после гибели — 'Обед для слизня'." +
                         $"\nПриятных иллюзий, Ваше Высокомерие!", ConsoleColor.Red);
-                    break;
-            }
-        }
-
-        public static void DisplayWelcomeMessage()
-        {
-            TechnicalSettings.ColorMessage("ПРИВЕТСТВУЮ, БУДУЩИЙ ТРУП! Я - ДУХ КОНСОЛИ, ТВОЙ ГИД В ЦИФРОВОМ АДУ. \nДАВАЙ СОЗДАДИМ ПЕРСОНАЖА КОТОРЫЙ ПОМРЕТ ВСРАТОЙ СМЕРТЬЮ!");
-            TechnicalSettings.ColorMessage("\nВЫБОР РАСЫ:");
-        }
-
-        public static void RaceOptionMessage()
-        {
-            TechnicalSettings.ColorMessage("\n1. ЧЕЛОВЕК - скучно, но универсально. Как латте.\n" +
-                "2. ЭЛЬФ - если хочешь быть изящным трупом. \n" +
-                "3. ДВАРФ - идеален для тех, кто мечтает о роскошном надгробии.", ConsoleColor.DarkYellow);
-        }
-
-        public static void UserChoiceRace(BaseCharacter playerHero,string selectRace)
-        {
-            switch (selectRace)
-            {
-                case "1":
-                    TechnicalSettings.ColorMessage("Ах, классика! Человек - существо, которое умудрилось изобрести колесо и кредиты в одном тысячелетии. " +
-                        "Твои главные таланты: умирать от бумажных порезов и размножаться в геометрической прогрессии." +
-                        " Надеюсь, ты принес вино? Ну тогда хотя бы умри эффектно!");
-                    selectRace = "людей";
-                    playerHero = new Human();
-                    break;
-                case "2":
-                    TechnicalSettings.ColorMessage("Ооо, изящный ушастый экспонат в лосинах! Ты так прекрасен, что даже мхи твоей могилы будут расти узором. " +
-                        "Помни: если проживешь 500 лет и не скажешь ни одного умного слова - это не сранная деменция, а традиция. " +
-                        "Спой нам песенку о вегетарианстве, пока гоблин готовит тебе клизму из стрелы!");
-                    selectRace = "эльфов";
-                    playerHero = new Elf();
-                    break;
-                case "3":
-                    TechnicalSettings.ColorMessage("*Откашливаюсь от пыли* " +
-                        "О, бородатый холодильник на ножках! Твои предки копали так глубоко, что однажды докопались до ада...и попросили скидку на уголь. " +
-                        "Совет: если застрянешь в тоннеле - не зови на помощь. " +
-                        "Так ты станешь вечным украшением семейного древа.");
-                    selectRace = "дварфов";
-                    playerHero = new Dwarf();
-                    break;
-                default:
-
                     break;
             }
         }
